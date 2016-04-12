e5eb586c8a53cd62a813a267c8860eec59663346
bharding2 <b.harding@gmail.com>
2016-04-12 15:50:32 -0700

    new test based on badass in class session

--- ben_harding/test/server_test.js
+++ ben_harding/test/server_test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const net = require('net');
 const fs = require('fs');
 const slothbearServer = require(__dirname + '/../server');
 
@@ -15,7 +16,7 @@ describe('tcp server', () => {
   });
 
   after(() => {
-    slothbearServer.close();
+    // slothbearServer.close();
   });
 
   it('should connect and write the request to a file', (done) => {
@@ -27,5 +28,19 @@ describe('tcp server', () => {
         chai.expect(newNumFiles).to.eql(oldNumFiles + 1);
         done();
       });
+  });
+});
+
+describe('another test', () => {
+  it('should log the right file', (done) => {
+    var client = net.connect({ port: 3000 }, () => {
+      client.write('hello test');
+    });
+    client.on('data', (chunk) => {
+      var fileContents = fs.readFileSync(__dirname + '/../' + chunk.toString() + '.txt');
+      chai.expect(fileContents.toString()).to.eql('hello test');
+      slothbearServer.close();
+      done();
+    });
   });
 });
